1f7caac9ddaa9be2ff21f2f71dd267569afeb6c3
Dylan Blokhuis <dylanblokhuis3@hotmail.com>
2024-01-13 22:03:38 +0100

    fix focus panic

--- examples/example_ui/src/lib.rs
+++ examples/example_ui/src/lib.rs
@@ -7,10 +7,10 @@ use tpaint::prelude::{
 pub fn app(cx: Scope) -> Element {
     render! {
         view {
-            class: "flex-col gap-y-10 w-full overflow-y-scroll scrollbar-default",
+            class: "flex-col gap-y-10 w-full overflow-y-scroll scrollbar-default bg-white",
 
             view {
-                class: "p-10 bg-indigo-300  gap-x-40 shrink-0 flex-col",
+                class: "p-10 gap-x-40 shrink-0 flex-col",
 
                 view {
                     class: "w-200 h-200 p-15 flex-nowrap bg-rose-900 overflow-y-scroll overflow-x-scroll scrollbar-default",
@@ -27,7 +27,7 @@ pub fn app(cx: Scope) -> Element {
                     class: "grow-0 w-full",
 
                     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ultrices vel urna nec dignissim. Nam ultrices elit id leo blandit sollicitudin. Phasellus dapibus augue ut augue condimentum, suscipit rhoncus ante elementum. Donec ut ante vel leo sodales iaculis in id neque. Praesent finibus risus egestas nisl fermentum, sit amet porttitor erat finibus. Aliquam nibh turpis, bibendum ut quam viverra, ullamcorper rutrum ex. Mauris arcu purus, venenatis vitae accumsan vitae, placerat id dolor. Mauris suscipit interdum lectus, ut ornare enim semper id. Sed et tempus nibh, vitae condimentum tortor. Quisque quis leo at sapien rutrum fermentum. Morbi iaculis, dui eleifend euismod malesuada, ligula ex semper velit, sit amet facilisis enim massa ut lacus. Ut sagittis tellus non sapien ornare feugiat. Curabitur a pretium massa. Integer pharetra risus vel quam mattis porta. Etiam suscipit rutrum cursus. Mauris aliquam ut ipsum et tincidunt."
-                }                
+                }
             }
 
             (0..10).map(|_| rsx! {
